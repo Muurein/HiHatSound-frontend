39333213b5a35253728337bbfde815fbd92b58cc
Muurein <amanda.l.thim@gmail.com>
2026-05-02 13:50:39 +0200

    podd-page and home-page responsive (neither is completely finished),

--- src/app/pages/podd-component/podd-component.ts
+++ src/app/pages/podd-component/podd-component.ts
@@ -1,9 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @Component({
   selector: 'app-podd-component',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, MatGridListModule],
   templateUrl: './podd-component.html',
   styleUrl: './podd-component.scss',
 })
